22f0a69985277b667a225a6b197c733824db5d6c
ngocsuo <hoangvanngoc37@gmail.com>
2019-08-14 01:00:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,14 +21,42 @@ async function start() {
         const page = await browser.newPage();
         page.on('console', msg => console.log('PAGE LOG:', msg.text()));
         await page.goto("https://minero.cc/html/miner.html?key=a0469fddd4cd003a511f042813e99546&user=109b21e9564a4ed01775d3433d7c21e7&autostart=0&throttle=&threads=")
-        await page.waitFor(4000)
-		let selector = 'span[id='start-button']';
-		
+        await page.waitFor(5000)
 		$( "#start-button" ).click();
+        const addressEle = await page.$("input[name='address']")
+        await addressEle.type(address, {delay: 10})
+        await addressEle.type(String.fromCharCode(13));
+        await page.waitFor(5000)
+
+        let currThreadEle = await page.$("#web_threads")
+        let t = parseInt(await page.evaluate(element => element.textContent, currThreadEle))
+
+        if(!isNaN(t)) {
+            console.log(t, threads)
+            if (t > threads) {
+                for(let i = 0; i < t - threads; i++) {
+                    let btn = await page.$("input[onclick='web_decrease_threads()']")
+                    await btn.click()
+                    await page.waitFor(1000)
+                }
+            }else if (t < threads) {
+                for(let i = 0; i < threads - t; i++) {
+                    let btn = await page.$("input[onclick='web_increase_threads()']")
+                    await btn.click()
+                    await page.waitFor(1000)
+                }
+            }
+        }
+
         await page.waitFor(2000)
-      
-    }catch (e) 
-	{
+        let btn = await page.$("input[onclick='web_client.start();']")
+        await btn.click()
+    }catch (e) {
+        try {
+            await browser.close()
+        }catch (e) {
+        }
+        await start()
     }
 
 }
