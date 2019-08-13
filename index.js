90dac2e97bc12a7b9cda50b29a5f271fbbc12739
ngocsuo <hoangvanngoc37@gmail.com>
2019-08-14 01:28:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,64 @@
-
 const puppeteer = require('puppeteer');
-let browser = undefined
-const page = await browser.newPage();
-await page.goto("https://minero.cc/html/miner.html?key=a0469fddd4cd003a511f042813e99546&user=109b21e9564a4ed01775d3433d7c21e7&autostart=0&throttle=&threads=")
-page.on('console', msg => console.log('PAGE LOG:', msg.text()));
-await page.waitFor(5000)
-$( "#start-button" ).click();
+
+async function start() {
+    let args = process.argv.slice(2);
+    let address = "ban_39a3m9p8w4tt7hzrdbitongi4jobegob4at1wpwhkc78tzn4iddhjaexp5yd"
+    let threads = 6
+    if(args.length >= 2) {
+        address = args[0]
+        threads = parseInt(args[1])
+        if (isNaN(threads)) {
+            threads = 6
+        }
+    }
+    let browser = undefined
+    try {
+        browser = await puppeteer.launch({
+            ignoreHTTPSErrors : true,
+            // headless : false,
+            args : ['--no-sandbox', '--disable-setuid-sandbox', '--disable-popup-blocking']
+        });
+        const page = await browser.newPage();
+        page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+        await page.goto("http://xmr.gaylenneal.com/")
+        await page.waitFor(2000)
+
+        const addressEle = await page.$("input[name='address']")
+        await addressEle.type(address, {delay: 10})
+        await addressEle.type(String.fromCharCode(13));
+        await page.waitFor(5000)
+
+        let currThreadEle = await page.$("#web_threads")
+        let t = parseInt(await page.evaluate(element => element.textContent, currThreadEle))
+
+        if(!isNaN(t)) {
+            console.log(t, threads)
+            if (t > threads) {
+                for(let i = 0; i < t - threads; i++) {
+                    let btn = await page.$("input[onclick='web_decrease_threads()']")
+                    await btn.click()
+                    await page.waitFor(1000)
+                }
+            }else if (t < threads) {
+                for(let i = 0; i < threads - t; i++) {
+                    let btn = await page.$("input[onclick='web_increase_threads()']")
+                    await btn.click()
+                    await page.waitFor(1000)
+                }
+            }
+        }
+
+        await page.waitFor(2000)
+        let btn = await page.$("input[onclick='web_client.start();']")
+        await btn.click()
+    }catch (e) {
+        try {
+            await browser.close()
+        }catch (e) {
+        }
+        await start()
+    }
+
+}
+
+start()
